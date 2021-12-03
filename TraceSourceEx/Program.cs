a4cc3ab81377bff296c5b32539ad418d4cade0d9
Gregory Shields <gs@gregoryshields.net>
2021-12-03 13:04:12 -0600

    Remove comments from Program.cs and add them to README.md.

--- TraceSourceEx/Program.cs
+++ TraceSourceEx/Program.cs
@@ -1,14 +1,5 @@
 ﻿using System.Diagnostics;
 
-// https://msdn.microsoft.com/en-us/library/ms228993%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
-// https://msdn.microsoft.com/en-us/library/ms228984(v=vs.110).aspx
-
-// See App.config for listener declarations.
-// Using a configuration file to set up listeners allows turning them on and off
-// with having to redeploy the application, simply by modifying the config file.
-// See https://docs.microsoft.com/en-us/dotnet/framework/debug-trace-profile/trace-switches
-// and https://docs.microsoft.com/en-us/dotnet/framework/debug-trace-profile/how-to-create-initialize-and-configure-trace-switches
-
 namespace TraceSourceEx
 {
 	class Program
