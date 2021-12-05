6d151583747bf9e921ef83f4b9d012981c970042
Gregory Shields <gs@gregoryshields.net>
2021-12-05 09:06:49 -0600

    Move comments from Program.cs to README.md.

--- README.md
+++ README.md
@@ -7,9 +7,40 @@ and
 
 See App.config for listener declarations.
 
-Using a configuration file to set up listeners allows turning them on and off
-with having to redeploy the application, simply by modifying the config file.
+Using a configuration file to set up listeners allows turning them on and off without having to redeploy the application, simply by modifying the config file.
 
 See [Trace Switches](https://docs.microsoft.com/en-us/dotnet/framework/debug-trace-profile/trace-switches)
 and [How to: Create, Initialize and Configure Trace Switches](https://docs.microsoft.com/en-us/dotnet/framework/debug-trace-profile/how-to-create-initialize-and-configure-trace-switches).
+
+
+## Discussion
+
+Without specifying any trace listeners or filters, trace messages will be written to the default trace listener, which is usually the Console window. To initialize the listeners and filters, we define them in our configuration file.
+
+To start with, the program creates a TraceSource object named __mySource from the trace source which is defined in App.config.
+
+Looking at App.config, we see that our TraceSource object has three properties defined:
+name="traceSourceEx",
+switchName="sourceSwitch",
+and switchType=SourceSwitch.
+
+We also see that traceSourceEx has two trace listeners declared, with the default trace listener having been removed.
+
+We can further see that App.config has exactly one trace switch declared with name "sourceSwitch".
+
+A trace source, represented by the [TraceSource class](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.tracesource), has exactly one trace switch, represented by its [Switch property](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.tracesource.switch?view=net-6.0#System_Diagnostics_TraceSource_Switch) which is of type [SourceSwitch](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.sourceswitch?view=net-6.0).
+
+
+### Trace Listeners
+
+The first listener is named "console" and is declared to be of type ConsoleTraceListener.
+It has exactly one filter of type EventTypeFilter which has a value of "Warning".
+
+The second listener...
+
+
+### Trace Switch
+
+Although there are 3 types of trace switches in .NET (BooleanSwitch, TraceSwitch, and SourceSwitch), the Switch property of TraceSource is always of type SourceSwitch. I verified this in the debugger's watch window.
+
 
--- TraceSourceEx/Program.cs
+++ TraceSourceEx/Program.cs
@@ -8,27 +8,6 @@ namespace TraceSourceEx
 
 		static Program() // Private static constructor.
 		{
-			// Create a TraceSource object named __mySource from the trace source which is defined in
-			// App.config.
-
-			// Looking at App.config, we see that our TraceSource object has name "traceSourceEx",
-			// switchName "sourceSwitch", and switchType SourceSwitch.
-
-			// We also see that traceSourceEx has two trace listeners declared and the default trace
-			// listener has been removed. A trace source can only have one trace switch, and we see
-			// that traceSourceEx has exactly one trace switch declared with name "sourceSwitch".
-
-			// Trace Listeners
-			// The first listener is named "console" and is declared to be of type ConsoleTraceListener.
-			// It has exactly one filter of type EventTypeFilter which has a value of "Warning".
-
-			// The second listener...
-
-			// Trace Switch
-			// There are 3 types of trace switches in .NET: BooleanSwitch, TraceSwitch, and SourceSwitch.
-			// I verified that the trace switch defined in traceSourceEx is of type SourceSwitch in the
-			// debugger's watch window.
-
 			__mySource = new TraceSource("traceSourceEx");
 		}
 
@@ -36,6 +15,23 @@ namespace TraceSourceEx
 		{
 			Activity1();
 
+			Activity2();
+
+			Activity3();
+
+			__mySource.Close();
+		}
+
+		static void Activity1()
+		{
+			// Raise events.
+			__mySource.TraceEvent(TraceEventType.Error, 1, "Error message.");
+			__mySource.TraceEvent(TraceEventType.Warning, 2, "Warning message.");
+			__mySource.TraceInformation("Informational message.");
+		}
+
+		static void Activity2()
+		{
 			// Change the event type for which tracing occurs.
 			// The console trace listener must be specified in the configuration file.
 
@@ -47,38 +43,21 @@ namespace TraceSourceEx
 			__mySource.Listeners["console"].Filter =
 				new EventTypeFilter(SourceLevels.Critical);
 
-			Activity2();
-
-			// Allow the trace source to send messages to listeners for all event types.
-			// This statement will override any settings in the configuration file.
-			__mySource.Switch.Level = SourceLevels.All;
-
-			// Restore the original filter settings.
-			__mySource.Listeners["console"].Filter = configFilter;
-			Activity3();
-
-			__mySource.Close();
-		}
-
-		static void Activity1()
-		{
-			// Create our trace source...
-			// Without identifying any trace listeners or filters, this code will cause trace messages
-			// to be written to the default trace listener. To initialize the listeners and filters, we
-			// define them in our configuration file.
-			__mySource.TraceEvent(TraceEventType.Error, 1, "Error message.");
-			__mySource.TraceEvent(TraceEventType.Warning, 2, "Warning message.");
-			__mySource.TraceInformation("Informational message.");
-		}
-
-		static void Activity2()
-		{
+			// Raise events.
 			__mySource.TraceEvent(TraceEventType.Critical, 3, "Critical message.");
 			__mySource.TraceInformation("Informational message.");
 		}
 
 		static void Activity3()
 		{
+			// Allow the trace source to send messages to listeners for all event types.
+			// This statement will override any settings in the configuration file.
+			__mySource.Switch.Level = SourceLevels.All;
+
+			// Restore the original filter settings.
+			__mySource.Listeners["console"].Filter = configFilter;
+
+			// Raise events.
 			__mySource.TraceEvent(TraceEventType.Error, 4, "Error message.");
 			__mySource.TraceInformation("Informational message.");
 		}
